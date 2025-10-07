0eac0c8ad5f0a1eb0e66dabafa6977dc4096330b
xo-m <202305562@student.polytechnic.bh>
2025-10-07 15:23:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").onsubmit=function() {alert("Your Inquiry Has Been Submitted."); return false;};
+
+document.getElementsByTagName("tr")[1].onclick=function () {alert("Monday class at 2-4 PM in room b20.104");};
+
+document.getElementsByTagName("tr")[2].onclick=function () {alert("Monday class at 2-4 PM in room b20.104");
